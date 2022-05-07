64e442347b99755a81f2d9592ba8497ec748565b
Fletcher Johnson <samuel.f.johnson96@gmail.com>
2022-05-06 15:41:41 -1000

    added pic of the day api

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import './App.css';
 import React, { useEffect, useState } from "react"
 import Accordion from 'react-bootstrap/Accordion'
+import Eventcard from './Components/Eventcard';
 
 function App() {
 
     let API_KEY = 'qnijM37ihRFu5YgP4NdXg2fn3xUIqVUqxcy5ZxZy'
     let startDate = '2021/05/05'
     let endDate = new Date()
-    let API_URL = `https://api.nasa.gov/DONKI/notifications?startDate=${startDate}&endDate=${endDate}&type=all&api_key=${API_KEY}`
+    let DONKI_URL = `https://api.nasa.gov/DONKI/notifications?startDate=${startDate}&endDate=${endDate}&type=all&api_key=${API_KEY}`
 
   const [events, setEvents] = useState([])
   const fetchData = () => {
-    fetch(API_URL)
+    fetch(DONKI_URL)
         .then(res => res.json())
         .then(data => {
           setEvents(data)
       })
   }
+
+
   useEffect(() => {
     fetchData()
   }, [])
 
+
+
   return (
       <div className="App">
-       
+      <Eventcard/>
        <Accordion >
           {events.map((event, index) => (
           
@@ -32,7 +37,7 @@ function App() {
                 <Accordion.Header>{event.messageType}|{event.messageID}
                 </Accordion.Header>
                 <Accordion.Body>
-                    {event.messageBody}
+                    <textarea style={{height: "50vh", width: "80vw"}}>{event.messageBody.split("#").join("\n")}</textarea>
                 </Accordion.Body>
               </Accordion.Item>
             
--- src/Components/Eventcard.jsx
+++ src/Components/Eventcard.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
 
 const Eventcard = () => {
+  let API_KEY = 'qnijM37ihRFu5YgP4NdXg2fn3xUIqVUqxcy5ZxZy'
+  let POD_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY} `
+
+  const [pictures, setPictures] = useState([])
+  const fetchPod = () => {
+    fetch(POD_URL)
+        .then(res => res.json())
+        .then(data => {
+          setPictures(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchPod()
+  }, [])
+
   return (
-    <div>Eventcard</div>
+    <div>
+       <img src={pictures.hdurl} style={{width: '100%'}}></img>
+
+    </div>
   )
 }
 
